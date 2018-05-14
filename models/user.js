dd136d0f6d646cd4a608d9e49161517c42ffa20e
lcolat <loic.colat@gmail.com>
2018-05-14 22:50:15 +0200

    update

--- models/user.js
+++ models/user.js
@@ -51,5 +51,28 @@ module.exports = function (sequelize, DataTypes) {
         return jwt.sign({user_id:this.id}, CONFIG.jwt_encryption, {expiresIn: expiration_time});
     };
 
+    User.verifyToken = (req,res) => {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        // decode token
+        if (token) {
+            // verifies secret and checks exp
+            jwt.verify(token, CONFIG.jwt_encryption, function(err, decoded) {
+                if (err) {
+                    return res.status(401).json({"error": true, "message": 'Unauthorized access.' });
+                }
+                req.decoded = decoded;
+                console.log("aaaa");
+                return true;
+            });
+        } else {
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                "error": true,
+                "message": 'No token provided.'
+            });
+        }
+    };
+
     return User;
 };
